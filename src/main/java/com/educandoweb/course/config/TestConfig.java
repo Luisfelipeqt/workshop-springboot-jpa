197394eccdd484be8fee1b8a6905384e5df406f9
Luis Felipe Rodrigues <luisfelipebr1995@gmail.com>
2023-01-16 10:41:42 -0300

    Quarto commit

--- src/main/java/com/educandoweb/course/config/TestConfig.java
+++ src/main/java/com/educandoweb/course/config/TestConfig.java
@@ -21,7 +21,9 @@ public class TestConfig implements CommandLineRunner {
 
         User u1 = new User(null, "Maria Brown", "maria@gmail.com", "999999999", "123456");
         User u2 = new User(null, "Alex Green", "alex@gmail.com", "88888888", "654321");
+        User u3 = new User(null, "Luis Felipe Rodrigues", "luisfelipebr1995@gmail.com", "(16)997198565", "987654321");
+
         System.out.println("Rodando com perfil Test");
-        userRepository.saveAll(Arrays.asList(u1, u2));
+        userRepository.saveAll(Arrays.asList(u1, u2, u3));
     }
 }
