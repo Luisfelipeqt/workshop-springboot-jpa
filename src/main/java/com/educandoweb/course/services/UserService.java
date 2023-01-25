53db275a3bf99e3e97e548c40f137e8ced4f53d1
Luisfelipqt <luisfelipebr1995@gmail.com>
2023-01-25 11:18:04 -0300

    Exception handlind delete

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -2,7 +2,12 @@ package com.educandoweb.course.services;
 
 import com.educandoweb.course.entities.User;
 import com.educandoweb.course.repositories.UserRepository;
+import com.educandoweb.course.services.exceptions.DatabaseException;
+import com.educandoweb.course.services.exceptions.ResourceNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
@@ -21,7 +26,7 @@ public class UserService {
 
     public User findById(Long id){
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj){
@@ -29,7 +34,15 @@ public class UserService {
     }
 
     public void delete(Long id){
-        repository.deleteById(id);
+    	try {
+    		repository.deleteById(id);
+    	}
+    	catch(EmptyResultDataAccessException e) {
+    		throw new ResourceNotFoundException(id);
+    	}
+    	catch(DataIntegrityViolationException e) {
+    		throw new DatabaseException(e.getMessage());
+    	}
     }
 
     public User update(Long id, User obj){
